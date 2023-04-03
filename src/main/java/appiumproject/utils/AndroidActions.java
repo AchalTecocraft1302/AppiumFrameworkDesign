bfc551017d592262993fe1fe468bee5ad91d3232
achaltecocraft <achal.tecocraft@gmail.com>
2023-04-03 18:44:35 +0530

    commit 3/4/23

--- src/main/java/appiumproject/utils/AndroidActions.java
+++ src/main/java/appiumproject/utils/AndroidActions.java
@@ -7,7 +7,6 @@ import com.google.common.collect.ImmutableMap;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,16 +16,11 @@ public class AndroidActions extends AppiumCommonUtils{
 	public Activity activity ;
 	AndroidDriver driver ;
 
+	
 	public AndroidActions(AndroidDriver driver) {
 
 		super(driver);
 		this.driver = driver;
-	}
-	public void swipeaction(WebElement element,String direction) {
-
-		//Swipe by javascriptExecutor :SwipeGesture
-		((JavascriptExecutor)driver).executeScript("mobile: swipeGesture",ImmutableMap.of("elementId",((RemoteWebElement) element).getId(),"direction",direction,"percent",0.75));
-		System.out.println("****** Image is swiped ********");
 	}
 	
 	public void scrollToCountry(String text) {
@@ -38,19 +32,11 @@ public class AndroidActions extends AppiumCommonUtils{
 
 	public void longPressAction(WebElement element) throws InterruptedException {
 		
-		((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",ImmutableMap.of("elementId",((RemoteWebElement) element).getId(),"duration",2000));
+		((JavascriptExecutor)driver).executeScript("mobile: longClickGesture",ImmutableMap.of("elementId",((WebElement) element),"duration",2000));
 		 System.out.println("******************* longpress action run ****************");
 		 Thread.sleep(3000);
 		 //RemoteWebElment is not cast with @FindBy so it change to WebElement
 	}
-	//sendkeys method - wait is included in this method
-	public void sendkeys(WebElement element,String text){
-		waitVisibiltyOfElement(element); //wait can change here as per requirement
-		element.clear();
-		element.sendKeys(text);
-	}
 
-	public void clickOn(WebElement element,String text){
-		waitVisibiltyOfElement(element); //wait can change here as per requirement
-	}
+
 }
