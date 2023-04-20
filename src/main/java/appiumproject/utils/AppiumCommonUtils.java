10c25593f6d79267eae9d0f0693cf3d9db5e4578
achaltecocraft <achal.tecocraft@gmail.com>
2023-04-20 19:30:38 +0530

    commit 20/4/21 - create setup method in common utils

--- src/main/java/appiumproject/utils/AppiumCommonUtils.java
+++ src/main/java/appiumproject/utils/AppiumCommonUtils.java
@@ -2,9 +2,21 @@ package appiumproject.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testinium.deviceinformation.DeviceInfo;
+import com.testinium.deviceinformation.DeviceInfoImpl;
+import com.testinium.deviceinformation.device.DeviceType;
+import com.testinium.deviceinformation.exception.DeviceNotFoundException;
+import com.testinium.deviceinformation.model.Device;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -17,8 +29,17 @@ import java.util.List;
 
 public abstract class AppiumCommonUtils {
 
+    public static String projectdir = System.getProperty("user.dir");
+    public AppiumDriverLocalService service;
+    public AppiumServiceBuilder builder;
+    public DesiredCapabilities cap;
+    public DeviceInfo deviceInfo ;
+    public Device device ;
     public AppiumDriver driver ;  //This grandparent class so AppiumDriver is also parent driver
     public WebDriverWait wait ;
+
+    //E-COMMERCE APP
+    public File app= new File( projectdir +"/src/test/resources/apps/General-Store.apk");
    
     public Double getStringToDouble(String stringtext) {
 
@@ -61,4 +82,61 @@ public abstract class AppiumCommonUtils {
         return element;
     }
 
+    public AppiumDriverLocalService startAppiumServer(){
+
+        //Build the Appium service
+        builder = new AppiumServiceBuilder();
+        builder.withIPAddress("127.0.0.1");
+        builder.withArgument(() -> "--port", "4723");
+        builder.withArgument(GeneralServerFlag.BASEPATH, "/wd/hub");
+        //127.0.0.1 is the  localhost normally resolves to the IPv4  127.0.0.1
+        builder.usingPort(4723); //Appium default port
+        builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
+        builder.withArgument(GeneralServerFlag.LOG_LEVEL,"debug");
+        builder.build();
+
+        //Start the Appium server with the builder
+        service = AppiumDriverLocalService.buildService(builder);
+        service.start();
+        System.out.println("**********************Appium Server Started via Java ************************************************");
+        return service;
+    }
+
+    public void getCapabilities() throws DeviceNotFoundException, IOException {
+        cap = new DesiredCapabilities();
+        deviceInfo = new DeviceInfoImpl(DeviceType.ANDROID);
+        deviceInfo.anyDeviceConnected();
+        device = deviceInfo.getFirstDevice();
+
+        System.out.println("Platform Name - " + device.getDeviceProductName());
+        System.out.println("Device UDID   - " + device.getUniqueDeviceID());
+        System.out.println("Product Verison - " + device.getProductVersion());
+        System.out.println("Model Number   - " + device.getModelNumber());
+        System.out.println("Buld Verison   - " + device.getBuildVersion());
+
+
+
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME,device.getDeviceProductName());
+        System.out.println("******************  get Platform name ****************");
+        cap.setCapability(MobileCapabilityType.UDID,device.getUniqueDeviceID());
+        System.out.println("******************  get UDID ****************");
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION,device.getProductVersion());
+        System.out.println("******************  get Platform Verison ****************");
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,device.getModelNumber());
+        System.out.println("******************  get device name (Model Number) ****************");
+
+        cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,60);
+
+        cap.setCapability("automationName", "UiAutomator2");
+        cap.setCapability(MobileCapabilityType.APP,app.getPath());
+
+        cap.setCapability("autoGrantPermissions", true);
+        cap.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
+
+
+
+
+        System.out.println("******************  capabilities are successfully taken ****************");
+
+    }
 }
--- src/test/java/appiumproject/testutils/ExtentManager.java
+++ src/test/java/appiumproject/testutils/ExtentManager.java
@@ -1,14 +1,16 @@
 package appiumproject.testutils;
 
+import appiumproject.testcases.SuperBaseClass;
+import appiumproject.utils.AppiumCommonUtils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-public class ExtentManager {
+public class ExtentManager extends AppiumCommonUtils {
 
     public static ExtentReports extentReports = new ExtentReports();
 
     public static ExtentReports createExtentReports(){
-        ExtentSparkReporter reporter = new ExtentSparkReporter(System.getProperty("user.dir")+"/src/test/java/appiumproject/testreports/extent-report.html");
+        ExtentSparkReporter reporter = new ExtentSparkReporter(projectdir +"/src/test/java/appiumproject/testreports/extent-report.html");
         reporter.config().setReportName("Sample Extent Report");
         extentReports.attachReporter(reporter);
         extentReports.setSystemInfo("Blog Name", "SW Test Academy");
